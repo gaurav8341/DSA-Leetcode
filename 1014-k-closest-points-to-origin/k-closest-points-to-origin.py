a0859561490ec092e302539e73ae6a4dd5cad7a6
Gaurav Rajput <rajput.gaurav8341@gmail.com>
2025-02-02 17:41:01 +0530

    Time: 98 ms (18.76%) | Memory: 23.5 MB (10.20%) - LeetSync

--- 1014-k-closest-points-to-origin/k-closest-points-to-origin.py
+++ 1014-k-closest-points-to-origin/k-closest-points-to-origin.py
@@ -6,11 +6,29 @@ class Solution:
         heap = []
 
         # while k > len
+        # for x, y in points:
+
+        #     dis = math.sqrt(x**2 + y**2)
+
+        #     heapq.heappush(heap, (dis, [x, y]))
+        
+        # res = []
+        # while k:
+        #     (dis, pts) = heapq.heappop(heap)
+        #     res.append(pts)
+        #     k -= 1
+        
+        # return res
+
+        ## The time complexity is nlogn for inserting points and klogn for getting values
+
+        # while k > len
         for x, y in points:
 
             dis = math.sqrt(x**2 + y**2)
+            heap.append((dis, [x, y]))
 
-            heapq.heappush(heap, (dis, [x, y]))
+        heapq.heapify(heap)
         
         res = []
         while k:
