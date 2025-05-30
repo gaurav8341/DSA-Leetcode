d6fae7ebf5428ae5ac43d92d7fe9181faf5b70bc
Gaurav Rajput <rajput.gaurav8341@gmail.com>
2025-05-31 02:25:29 +0530

    Time: 35 ms (93.95%) | Memory: 34.7 MB (18.86%) - LeetSync

--- 131-palindrome-partitioning/palindrome-partitioning.py
+++ 131-palindrome-partitioning/palindrome-partitioning.py
@@ -34,22 +34,34 @@
 
     #     return res
 
+"""
+In below soln we precaclculate if string is palindorme or not
+
+"""
+
 class Solution:
     def partition(self, s: str) -> List[List[str]]:
-        def is_palindrome(sub):
-            return sub == sub[::-1]
+        n = len(s)
+        dp = [[False] * n for _ in range(n)]
+        for i in range(n):
+            dp[i][i] = True
+        for length in range(2, n + 1):
+            for i in range(n - length + 1):
+                j = i + length - 1
+                if s[i] == s[j]:
+                    if length == 2 or dp[i + 1][j - 1]:
+                        dp[i][j] = True
 
         def backtrack(start, path):
-            if start == len(s):
+            if start == n:
                 result.append(path[:])
                 return
-            for end in range(start + 1, len(s) + 1):
-                if is_palindrome(s[start:end]):
-                    backtrack(end, path + [s[start:end]])
+            for end in range(start, n):
+                if dp[start][end]:
+                    backtrack(end + 1, path + [s[start:end+1]])
 
         result = []
         backtrack(0, [])
         return result
-
             
             
