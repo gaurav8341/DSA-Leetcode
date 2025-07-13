24b4064eced96161a6583a7bd90c87c97ec10e72
Gaurav Rajput <rajput.gaurav8341@gmail.com>
2025-07-13 23:01:05 +0530

    Time: 79 ms (52.83%) | Memory: 34.1 MB (28.68%) - LeetSync

--- 2497-maximum-matching-of-players-with-trainers/maximum-matching-of-players-with-trainers.py
+++ 2497-maximum-matching-of-players-with-trainers/maximum-matching-of-players-with-trainers.py
@@ -3,22 +3,17 @@ class Solution:
         # sort the arrays
         players.sort()
         trainers.sort()
-        print(players, trainers)
         # For each player we need to find the trainer with least training ability
         trdx = 0 # trainer idx 
         pdx = 0
         # We will increament this if either there is a match or trainer is not sufficient
         pairings = 0
         while pdx < len(players) and trdx < len(trainers):
-            p = players[pdx]
-            print(p, trainers[trdx], trdx, )
-
-            if trainers[trdx] >= p:
+            if trainers[trdx] >= players[pdx]:
                 pairings += 1
-                trdx += 1
                 pdx += 1
-            else:
-                trdx += 1
+            # else:
+            trdx += 1
         return pairings
 
         
