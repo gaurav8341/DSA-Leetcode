a8dbec386d4c6cad0fc0b965d263591a3998f001
Gaurav Rajput <rajput.gaurav8341@gmail.com>
2025-07-18 23:38:28 +0530

    Time: 0 ms (100.00%) | Memory: 17.7 MB (78.28%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.py
+++ 70-climbing-stairs/climbing-stairs.py
@@ -5,14 +5,23 @@ class Solution:
         # each time 1 / 2 steps
         # distinct ways to climb to top
 
-        dp = [-1] * (n + 1)
-        dp[0] = 1 # if n =0 or n = 1 there is only one way to climb the stairs
-        dp[1] = 1
+        # dp = [-1] * (n + 1)
+        # dp[0] = 1 # if n =0 or n = 1 there is only one way to climb the stairs
+        # dp[1] = 1
 
-        for i in range(2, n+1): 
-            dp[i] = dp[i-1] + dp[i-2]
+        # for i in range(2, n+1): 
+        #     dp[i] = dp[i-1] + dp[i-2]
         
-        return dp[n]
+        # return dp[n]
+        curr_i = 0
+        prev2 = 1
+        prev = 1
+        for i in range(2, n+1):
+            curr_i = prev2 + prev
+            prev2 = prev
+            prev = curr_i
+        
+        return prev
         
         # steps = 0
         # res = 0
