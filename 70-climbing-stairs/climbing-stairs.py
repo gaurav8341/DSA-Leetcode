245b5db5e231750149c4249814b3094f229fb32f
Gaurav Rajput <rajput.gaurav8341@gmail.com>
2025-07-18 23:33:53 +0530

    Time: 0 ms (100.00%) | Memory: 17.9 MB (29.93%) - LeetSync

---
+++ 70-climbing-stairs/climbing-stairs.py
@@ -0,0 +1,30 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        # climbing a staircase
+        # n steps to top
+        # each time 1 / 2 steps
+        # distinct ways to climb to top
+
+        dp = [-1] * (n + 1)
+        dp[0] = 1 # if n =0 or n = 1 there is only one way to climb the stairs
+        dp[1] = 1
+
+        for i in range(2, n+1): 
+            dp[i] = dp[i-1] + dp[i-2]
+        
+        return dp[n]
+        
+        # steps = 0
+        # res = 0
+
+        # def backtrack():
+        #     if steps == n:
+        #         res += 1
+        #         return
+            
+        #     if n - steps:
+
+
+        #     steps += 1
+        #     backtrack()
+        #     stesp += 1
