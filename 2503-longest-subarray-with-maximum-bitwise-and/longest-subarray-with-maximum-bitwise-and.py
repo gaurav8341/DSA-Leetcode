c960f1ec7f29931ce832922a7fb3b25cfe946329
Gaurav Rajput <rajput.gaurav8341@gmail.com>
2025-07-30 14:43:23 +0530

    Time: 61 ms (56.21%) | Memory: 30.8 MB (12.43%) - LeetSync

--- 2503-longest-subarray-with-maximum-bitwise-and/longest-subarray-with-maximum-bitwise-and.py
+++ 2503-longest-subarray-with-maximum-bitwise-and/longest-subarray-with-maximum-bitwise-and.py
@@ -9,21 +9,33 @@ class Solution:
         # 4&4&3 = 2
         # 4&4&1 = 0
         # inshort the question is to find the largest subarray contaning the max element.
-        max_elem = 0
-        max_count, count = 0, 0
-        for  i in nums:
-            if i > max_elem:
-                # new max is found also reset the max_count
-                max_elem = i
-                count = 0
-                max_count = 0
+        # max_elem = 0
+        # max_count, count = 0, 0
+        # for  i in nums:
+        #     if i > max_elem:
+        #         # new max is found also reset the max_count
+        #         max_elem = i
+        #         count = 0
+        #         max_count = 0
             
-            if i < max_elem:
-                count = 0
-            elif i == max_elem:
-                count += 1
+        #     if i < max_elem:
+        #         count = 0
+        #     elif i == max_elem:
+        #         count += 1
 
-            max_count = max(max_count, count)
+        #     max_count = max(max_count, count)
 
 
-        return max_count
+        # return max_count
+        maxi = max(nums)
+        max_len = 0
+        length = 0
+
+        for num in nums:
+            if num == maxi:
+                length += 1
+                max_len = max(max_len, length)
+            else:
+                length = 0
+
+        return max_len
