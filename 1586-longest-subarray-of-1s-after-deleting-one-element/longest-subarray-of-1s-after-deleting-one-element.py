ed568352ab36de975f0b6f5087887aa46e55ef7b
Gaurav Rajput <rajput.gaurav8341@gmail.com>
2025-08-24 16:55:44 +0530

    Time: 47 ms (79.18%) | Memory: 21.8 MB (27.52%) - LeetSync

--- 1586-longest-subarray-of-1s-after-deleting-one-element/longest-subarray-of-1s-after-deleting-one-element.py
+++ 1586-longest-subarray-of-1s-after-deleting-one-element/longest-subarray-of-1s-after-deleting-one-element.py
@@ -6,7 +6,6 @@ class Solution:
         idx_0 = l if nums[l] == 0 else -1 
         _max = 0
         for r in range(1, len(nums)):
-            # while nums[l] == 0:
             # print(r, l, idx_0)
             if nums[r] == 0:
                 # check if prev 0 exists if not do nothing 
@@ -14,5 +13,5 @@ class Solution:
                 if idx_0 >= 0:
                     l = idx_0 + 1
                 idx_0 = r
-            _max = max(_max, r - l) # as we are removing an elem 
+            _max = max(_max, r - l) # as we are removing an elem we are not adding +1 
         return _max
