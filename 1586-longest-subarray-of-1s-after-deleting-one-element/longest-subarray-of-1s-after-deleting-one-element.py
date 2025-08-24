d2dabccff48940e64ebdebb4d240416cd020262b
Gaurav Rajput <rajput.gaurav8341@gmail.com>
2025-08-24 16:57:21 +0530

    Time: 64 ms (54.73%) | Memory: 21.8 MB (27.52%) - LeetSync

--- 1586-longest-subarray-of-1s-after-deleting-one-element/longest-subarray-of-1s-after-deleting-one-element.py
+++ 1586-longest-subarray-of-1s-after-deleting-one-element/longest-subarray-of-1s-after-deleting-one-element.py
@@ -2,16 +2,33 @@ class Solution:
     def longestSubarray(self, nums: List[int]) -> int:
         # idea is parse the list
 
-        l = 0
-        idx_0 = l if nums[l] == 0 else -1 
-        _max = 0
-        for r in range(1, len(nums)):
-            # print(r, l, idx_0)
-            if nums[r] == 0:
-                # check if prev 0 exists if not do nothing 
-                # if it does then reset l
-                if idx_0 >= 0:
-                    l = idx_0 + 1
-                idx_0 = r
-            _max = max(_max, r - l) # as we are removing an elem we are not adding +1 
-        return _max
+        # l = 0
+        # idx_0 = l if nums[l] == 0 else -1 
+        # _max = 0
+        # for r in range(1, len(nums)):
+        #     # print(r, l, idx_0)
+        #     if nums[r] == 0:
+        #         # check if prev 0 exists if not do nothing 
+        #         # if it does then reset l
+        #         if idx_0 >= 0:
+        #             l = idx_0 + 1
+        #         idx_0 = r
+        #     _max = max(_max, r - l) # as we are removing an elem we are not adding +1 
+        # return _max
+
+        # class Solution:
+    # def longestSubarray(self, nums: List[int]) -> int:
+        left, zeros, res = 0, 0, 0
+        
+        for right in range(len(nums)):
+            if nums[right] == 0:
+                zeros += 1
+            
+            while zeros > 1:
+                if nums[left] == 0:
+                    zeros -= 1
+                left += 1
+            
+            res = max(res, right - left)
+        
+        return res
